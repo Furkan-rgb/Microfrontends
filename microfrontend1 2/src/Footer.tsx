29387a997a9243e477d5fa9da6e5f62ab913c7e5
Öztürk,Furkan F <50831308+Furkan-rgb@users.noreply.github.com>
2022-12-21 13:36:02 +0100

    Update

---
+++ microfrontend1 2/src/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="text-3xl text-white bg-blue-500 p5 font bold">
+      This Footer is from Microfrontend 1
+    </div>
+  );
+}
